52b26332e68ab4591cfca0c07d30b1aa6a7d71b1
NetKalon <Netkalon@gmail.com>
2020-10-10 08:40:15 -0700

    1st commit

--- CoreAngular/Startup.cs
+++ CoreAngular/Startup.cs
@@ -25,6 +25,7 @@ namespace CoreAngular
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
+
             });
         }
 
